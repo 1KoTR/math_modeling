d22e47ecdaeddc6c2fac7da273666ecbdf57f86d
1KoTR <61472343+1KoTR@users.noreply.github.com>
2020-12-14 18:38:10 +0200

    Add files via upload

--- Project.py
+++ Project.py
@@ -44,28 +44,34 @@ H = (V_0**2*np.sin(alpha)**2)/(2*g) # Высота подъёма шара.
 # ---------- Пространство для анимации ----------
 fig, ax = plt.subplots()
 
-ax.set_xlim(0, L + L/10) # Пределы изменения X.
-ax.set_ylim(0, H + H/10) # Пределы изменения Y.
-
-# ----- Украшения -----
-ax.set_xlabel(f'Дальность (max: {np.around(L, 3)}), м')
-ax.set_ylabel(f'Высота (max: {np.around(H, 3)}), м')
-
-ax.legend()
+ax.set_xlim(0, max([L + L/10, H + H/10])) # Пределы изменения X.
+ax.set_ylim(0, max([L + L/10, H + H/10])) # Пределы изменения Y.
 
 # ---------- Анимирумые объекты ----------
-objLine, = plt.plot([], [], '--', lw=1, color='k', label='trajectory') # Траетория полёта шара.
-objBall, = plt.plot([], [], '.', color='r', ms=20, label='ball') # Шар.
+objLine, = plt.plot([], [], '--', lw=1, color='k') # Траетория полёта шара.
+objBall, = plt.plot([], [], '.', color='r', ms=25) # Шар.
+
+objVector_X, = plt.plot([], [], '--', lw=2, color='r', marker='>', ms=5)
+objVector_Y, = plt.plot([], [], '--', lw=2, color='r', ms=5)
 
 # ---------- Функция подстановки параметров ----------
 xLine, yLine = [], []
-def update(t):
-    xLine.append(V_0*np.cos(alpha)*t) # Рассчитывает X
-    yLine.append(V_0*np.sin(alpha)*t - (g*t**2)/2) # Рассчитывает Y
+def Update(t):
+    X = V_0*np.cos(alpha)*t # Рассчитывает X
+    Y = V_0*np.sin(alpha)*t - (g*t**2)/2 # Рассчитывает Y
     
+    xLine.append(X)
+    yLine.append(Y)
     objLine.set_data(xLine, yLine)
 
     objBall.set_data(xLine[::-1][0], yLine[::-1][0])
+    
+    X_2 = X + V_0*np.cos(alpha)*max([H, L])/(5*V_0)
+    objVector_X.set_data([X, X_2], Y)
+    
+    Y_2 = Y + (V_0*np.sin(alpha) - g*t)*max([H, L])/(5*V_0)
+    objVector_Y.set_marker('^' if (Y_2 > Y) else 'v')
+    objVector_Y.set_data(X, [Y, Y_2])
 
     # ----- Украшения -----
     ax.set_title(f'Время: {np.around(t, 3)} c' + \
@@ -73,19 +79,19 @@ def update(t):
                  f'\nВысота от начала: {np.around(yLine[::-1][0], 3)} м')
 
 # ----- Украшения -----
-ax.set_xlabel(f'Дальность (max: {np.around(L, 3)}), м')
+ax.set_xlabel(f'Расстояние (max: {np.around(L, 3)}), м')
 ax.set_ylabel(f'Высота (max: {np.around(H, 3)}), м')
-ax.legend()
-ax.grid()
+ax.set_aspect('equal', adjustable='box') # Делает график квадратным.
+ax.grid() # Создаёт сетку на графике
 
 # ---------- Вывод ----------
 print(f'\nВремя полёта шара: {np.around(T, 3)} с')
-print(f'Максимальная высота подъёма шара: {np.around(H, 3)} м')
+print(f'Высота подъёма шара: {np.around(H, 3)} м')
 print(f'Дальность полёта шара: {np.around(L, 3)} м')
 
-t = np.linspace(0, T, 100)
-anim = FuncAnimation(fig, update, t)
+t = np.linspace(0, T, 60)
+anim = FuncAnimation(fig, Update, t)
 try:
-    anim.save(f'trajectory[{outAlpha},{V_0}].gif', writer='pillow', fps=30) # Сохранение gif файла в папке с программой
+    anim.save(f'trajectory[{outAlpha},{V_0}].gif', writer='pillow', fps=50) # Сохранение gif файла в папке с программой
 except:
     print('Ошибка! Не удалось сохранить gif файл.')
